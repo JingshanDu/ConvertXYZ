6552fbf2f648c91fa981fe910b6c90814f1648de
Jingshan S. Du <dujingshan@live.com>
2021-10-28 02:42:43 -0700

    Added info printing and enhanced error handling

--- Program.cs
+++ Program.cs
@@ -50,15 +50,33 @@ namespace ConvertXYZ
         
         private static void Run(Options option)
         {
+            // Global error indicator; if any file failed
+            bool IsGlobalError = false;
             try
             {
                 // Load the periodic table
                 var objPeriodicTable = PeriodicTable.Load();
+
                 foreach (string strInputFilePath in option.InputFiles)
                 {
+                    // Print some explanary information
+                    string strMode = "forward";
+                    string strExplainMode = "standard to computem xyz.";
+                    if (option.IsReverse)
+                    {
+                        strMode = "reverse";
+                        strExplainMode = "computem to standard xyz.";
+                    }
+                    string strVersionNum = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                    Console.WriteLine("ConvertXYZ " + strVersionNum + " operating in the " + strMode + " mode, converting from " + strExplainMode);
+
+                    // Error state; if one line fails, stop processing this file and move to the next
+                    bool IsFileError = false;
+
                     // Set up the input file
                     Console.WriteLine("Processing file: " + strInputFilePath);
                     var txtXYZ = new List<string>(File.ReadAllLines(strInputFilePath));
+
                     // Set up the output file
                     string strOutputDirectory = String.IsNullOrEmpty(option.OutputDirectory) ? Path.GetDirectoryName(strInputFilePath) : option.OutputDirectory;
                     string strOutputAppend = option.IsReverse ? "-std.xyz" : "-computem.xyz";
@@ -83,10 +101,19 @@ namespace ConvertXYZ
                         // Parse and process each XYZ record
                         foreach (string line in txtXYZ)
                         {
-                            string[] atom = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                            if (string.IsNullOrEmpty(line)) continue;
                             // A line "-1" should be treated as the end of file in computem
                             if (line.Equals("-1")) break;
+                            // Split the line into segments based on spaces
+                            string[] atom = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                            if (string.IsNullOrEmpty(line)) continue;
+                            // If the format is wrong, print the failed line and stop processing this file
+                            if (atom.Length != 6)
+                            {
+                                StopLine(line);
+                                IsFileError = true;
+                                IsGlobalError = true;
+                                break;
+                            }
                             atom[0] = objPeriodicTable[Int32.Parse(atom[0])].Symbol;
                             // Remove occupancy and thermal vibration and add this line to the output
                             var newline = string.Join("   ", atom[0], atom[1], atom[2], atom[3]);
@@ -94,6 +121,7 @@ namespace ConvertXYZ
                             if (option.IsVerbose) Console.WriteLine(newline);
                             intTotalAtoms++;
                         }
+                        if (IsFileError) break;
                         txtXYZNew[0] = intTotalAtoms.ToString();
                     }
                     else
@@ -112,8 +140,17 @@ namespace ConvertXYZ
                         // Parse and process each XYZ record
                         foreach (string line in txtXYZ)
                         {
+                            // Split the line into segments based on spaces
                             string[] atom = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
                             if (string.IsNullOrEmpty(line)) continue;
+                            // If the format is wrong, print the failed line and stop processing this file
+                            if (atom.Length != 4)
+                            {
+                                StopLine(line);
+                                IsFileError = true;
+                                IsGlobalError = true;
+                                break;
+                            }
                             atom[0] = objPeriodicTable[atom[0]].AtomicNumber.ToString();
                             // Add occupancy (1) and thermal vibration coeff and add this line to the output
                             var newline = string.Join("   ", string.Join("   ", atom), '1', option.ThermalVibration.ToString());
@@ -124,6 +161,7 @@ namespace ConvertXYZ
                             y.Add(Convert.ToSingle(atom[2]));
                             z.Add(Convert.ToSingle(atom[3]));
                         }
+                        if (IsFileError) break;
                         // Get the x y z boundaries as the cell size for the second line of the output
                         x.Sort();
                         y.Sort();
@@ -142,8 +180,23 @@ namespace ConvertXYZ
             catch(Exception e)
             {
                 Console.WriteLine(e.Message);
+                IsGlobalError = true;
             }
             Console.WriteLine("All tasks completed.");
+            if(IsGlobalError)
+            {
+                Console.WriteLine("There were error(s) during the conversion.");
+            }
+            else
+            {
+                Console.WriteLine("No error detected.");
+            }
+        }
+
+        private static void StopLine(string strLine)
+        {
+            Console.WriteLine("File format is wrong. Cannot process this line: " + strLine);
+            Console.WriteLine("This file is skipped.");
         }
     }
 }
